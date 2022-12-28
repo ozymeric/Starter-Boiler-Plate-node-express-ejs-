13c79590344ffa1c4994e3b33101e3233eed6c38
ozymeric <mail.ozanmeric@gmail.com>
2022-12-28 23:20:34 +0300

    this git folder can be used as a starter boiler plate with all the includes, views, util, routes, data, public folders and all the necessary node packages.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const path = require("path");
 
 const defaultRoutes = require("./routes/default");
 
+const dataFile = require("./util/dataFileFetch");
+
 // #########################################################
 // #########################################################
 // #########################################################
@@ -24,6 +26,11 @@ app.use("/", defaultRoutes);
 // #########################################################
 // #########################################################
 // #########################################################
+
+app.get("/filedata", function (req, res) {
+  const fileData = dataFile.fetchFileData();
+  res.render("datacontent", { file: fileData });
+});
 
 app.listen(3000, function () {
   console.log("Server is up and running on port: 3000");
